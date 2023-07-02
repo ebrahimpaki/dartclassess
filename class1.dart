301d6128f7d9f5f4f3798f3c00c6d7f78e815fe3
ebrahimpaki <ebrahimpaki@gmail.com>
2023-07-03 00:33:37 +0500

    class1.dart & class2.dart

---
+++ class1.dart
@@ -0,0 +1,121 @@
+void main() {
+
+print("class 1 file");
+
+// string example
+
+// print("Hello World"); 1
+// print("my name is Ibrahim");
+
+  // print("""      MUHAMMAD IBRAHIM 
+  //     Saylani Mass IT Traning 
+  //     KARACHI.""");
+// print("apple = 35");
+// print("graps = 10");
+// print("--------------");
+// print("Total= 45");
+
+// String name = "ALI";
+// String adress = "Street No 123 abc karachi";
+// String date = "28-March-2021";
+// String email = "bilalrehmani080808@gmail.com";
+// print(name);
+// print(adress);
+// print(date);
+// print(email);
+
+// variable example
+  // var a = 30;
+  // print(a);
+  // print("a");
+
+// int age = 19;
+  // double temperature = 16.5;
+  // print(age);
+  // print(temperature);
+
+// num age =19;
+// num temperature =18.5;
+
+// print(age);
+// print(temperature);
+
+  // num apple = 35;
+  // num grape = 10;
+  // num total = apple + grape;
+  // print("Apples = ${apple}");
+  // print("Grapes = ${grape}" );
+  // print("---------------");
+  // print("Total = ${total}"); 
+
+// var age = 32;
+// print(age);
+
+// is code mai error aaye ga q k 2 varialbe aik jese assign nahi kr sakte
+// int age = 12;
+// String name = "Ibrahim";
+//  int age = 25;
+// print(age);
+
+// is int k variable mai age 31 print hogi q k is ki data type update ho gaye hai
+// int age = 23;
+// String name = "Ibrahim";
+// age= 31;
+// print(age);
+
+//  is mai error aay ga q k assign se pehle hum print nahi de sakte
+// print(age);
+//  int age =26;
+
+// String name = "Ibrahim";
+// print(name);
+
+// double mai point wali value show krta hai
+  // double age = 12;
+  // print(age);
+
+  // int age1 = 13;
+  // int age2 = age1;
+  // print(age2);
+
+// is datatype mai error aay ga q k int double mai assign nahi ho sakta
+  // int age1 = 13;
+  // double age2 = age1;
+  // print(age2);
+
+// num sub mai use ho jaye ga
+  // int age1 = 13;
+  // num age2 = age1;
+  // print(age2);
+
+// marksheet
+  // print("SAYLANI MASS IT TRANING");
+  // print("RESULT CARD");
+  // print("STUDENT NAME = MUHAMMAD IBRAHIM");
+  // print("----------------------------------");
+  // num english=20;
+  // num history= 18;
+  // num science=19.5;
+  // num biology = 16.8;
+  // num urdu = 18.5;
+  // num  obtainedMark = english+history+science+biology+urdu;
+  // num total = 100;
+
+  //  num percentage = total*obtainedMark/100;
+  //  print("Total $total");
+  //  print("percentage $percentage");
+
+  // practise
+//   {
+    
+//   var a = num.parse("1");  
+//   var b = num.parse("2.34");  
+  
+//   var c = a+b;   
+//   print("Product = ${c}");
+
+// }  
+
+
+  
+}
---
+++ class2.dart
@@ -0,0 +1,152 @@
+void main() {
+  //BOOLEAN (true , false)
+  // bool abc = 12>8;          // 1 answr true
+  //  print(abc);
+
+  // bool abc = 12 < 8;          //2   answer false
+  // print(abc);
+
+  // bool calculation;             //3
+  // calculation=12>5;
+  // print(calculation);
+
+  //  int num1 = 34;            // 4
+  //  int num2 = 43;
+  //  bool result = (num1>num2);
+  //  print(result);
+
+  // int age = 18;             // 5
+  // bool result = age >=18;
+  // print(result);
+
+  // int age = 16;            // 6
+  // bool result = age >=18;
+  // print(result);
+
+  //  int age = 18;             // 7
+  //  bool result = age !=18;
+  //  print(result);
+
+  // int age = 17;                        // 7
+  // bool result = age != 18;
+  // print(result);
+
+  // OPERATORS:
+
+//  int num1 = 34;       // additional
+//  int num2 = 43;
+//  print(num1+num2);
+
+  // int num1 = 64;       // subtract
+  // int num2 = 43;
+  // print(num1-num2);
+
+  // int num1 = 14;       // multiply
+  // int num2 = 13;
+  // print(num1*num2);
+
+  // int num1 = 34;            // multyply is mai hum ne aik hum ne aik variable mai assign kr dia hai
+  // int num2 = 43;
+  // num result = (num1 * num2);
+  // print(result);
+
+  //  int num1 = 36;       // divided
+  //  int num2 = 4;
+  //  print(num1/num2);
+
+  //  int num1 = 34;       // ~divided is operator ka naam divide integer  hai
+  //  int num2 = 3;
+  //  print(num1~/num2);
+
+  // int num1 = 34;            // ~divided is mai hum ne aik hum ne aik variable mai assign kr dia hai
+  // int num2 = 4;
+  // num result = (num1 ~/ num2);
+  // print(result);
+
+  //  int num1 = 34;            // Remender
+  //  int num2 = 4;
+  //  num result = (num1 % num2);
+  //  print(result);
+
+  // LOGICAL OPERATOR
+  // String email = "abcd@gmail.com";     //
+  // String password = "12345";
+  // bool condition = email== "abcd@gmail.com";
+  // print(condition);
+
+  //  String email = "abcd@gmail.com";        // && operator mai dono condition same honi chayee
+  //  String password = "12345";
+  //  bool condition = email== "abcd@gmail.com" && password == "123456";
+  //  print(!condition);
+
+  //  String email = "abcd@gmail.com";        // && operator mai dono condition same honi chayee
+  //  String password = "12345";
+  //  bool condition = email== "abcd@gmail.com" && password == "123456";
+  //  print(condition);
+
+  //  String email = "abcd@gmail.com";        // || operator mai dono mai se aik condition same honi chayee
+  //  String password = "12345";
+  //  bool condition = email== "abcd@gmail.com" || password == "123456";
+  //  print(condition);
+
+  //  String email = "abcd@gmail.com";        // || operator mai dono mai se aik condition same honi chayee
+  //  String password = "12345";
+  //  bool condition = email== "abcd@gmail.com" || password == "12345";
+  //  print(!condition);
+
+  // int abc = 34;
+  // int a = 21;
+  // int q = 12;
+  // int k = 8;
+  // int i = a - q; // 9
+  // bool w = abc > a && !(a < abc);
+  // // true   &&    !(true)
+  // // true   &&      false
+  // // false
+  // bool e = !w || q != k;
+  // //     !false || true
+  // //      true   || true
+  // //      true
+
+  // bool o = e && i > 4;
+  // // true   &&      true
+  // // true
+
+
+  //  int num1 =19;
+  //  print(num1 == "19");
+
+  // int num = 19;
+  // bool abc = num == "19";
+  // print (abc);
+
+  // print(!(!o || e));
+  // // !(!true  || true )
+  // // !(false  || true)
+  // //  !(true)
+  // // false
+
+  // condition if else
+//  int num1= 12;
+//  int num2 = 8;
+//  if(num1>num2){
+//   print("num1 is greater then num2");
+//  }
+//  else{
+//   ("num2 is greater then num 1");
+//  }
+
+  // int num1 = 12;
+  // int num2 = 12;
+  // if (num1 > num2) {
+  //   print("num1 is greater then num2");
+  // } 
+  // else if ( num1 == num2){
+  //  print("num1 is equal to num 2");
+  //  }
+  //  else{
+  //   print("num2 is greater then num1");
+  //  }
+
+  
+}
