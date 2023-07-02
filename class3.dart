09d19e8335735c335b7ff4eade733c2dc10f9739
ebrahimpaki <ebrahimpaki@gmail.com>
2023-07-03 02:04:41 +0500

    create class3.dart

---
+++ class3.dart
@@ -0,0 +1,23 @@
+void main(){
+  // String a = "sub se pehle github download kre ge";
+  // String b = "phir id banaye ge yani apna account banaye ge git mai ";
+  // String c = """jab bhi hum git mai new account banate hai us mai us waqt sirf ye 2 file chalate hai
+  //             system mai wo ye hai ye file sirf 1st time chalye ge""";
+  //  String d = "git config --global user.name ebrahimpaki";
+  //  String e = "git config --global user.email ebrahimpaki@gmail.com";
+  //  String f = "jab hum cmd mai kaam kre ge to cd desktop is k baad cd folder name  dart run";
+  //  String g = "git status ye file hum use nahi kre ge ye file sirf status check krne k liye hai";
+  //  String h = "humesha hum yaha se shuru kre ge (git init) git ko initilize kr dega project";
+  //  String i = "git add .  yaaa   git add file name        is se file add ho jaye gi";
+  //  String j =  "git commit -m  message";  // message mai hum koi bhi ya file name bhi de sakte ha            
+  //  String k = "git remote add origin repolink";  // ye link github se copy past kre ge
+  //  String l = "git push -u origin master";
+  // print(a+b+c+d+e+f+g+h+i+j+k+l); 
+
+  String m =("project ko update krne ka tariqa");
+  String n = """jab kisi file ko change kre ge to m likha hua aay ga or new file add kre ge to a
+              likha hua a jaye ga""";
+  String o = "git commit -m creat class 3.dart & update class2.dart" ;
+  String p =  "git push     update krne k liye pura push -u nahi likhna q k branch pata hai isko ";
+ print(m+n+o+p);
+}
