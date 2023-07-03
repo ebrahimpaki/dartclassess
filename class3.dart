2db3dc92516a45cfe9ca86d9e755b85b38249885
ebrahimpaki <ebrahimpaki@gmail.com>
2023-07-03 06:43:41 +0500

    update class3.dart

--- class3.dart
+++ class3.dart
@@ -18,7 +18,8 @@ void main(){
   String n = """jab kisi file ko change kre ge to m likha hua aay ga or new file add kre ge to a
               likha hua a jaye ga""";
   String o =  "git add .";
-  String p = "git commit -m create class3.dart & update class2.dart" ;  //-m k baad commit inverted comma mai likhna haiadd
+  String p = "git commit -m (message)" ;           // message ki esample niche de hue hai
+  //create class3.dart & update class2.dart" ;  //-m k baad commit inverted comma mai likhna haiadd
   String q =  "git push     update krne k liye pura push -u nahi likhna q k branch pata hai isko ";
  print(m+n+o+p+q);
 }
