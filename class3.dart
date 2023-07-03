5ed807c43b3da5e3bce0b8be64be4cb361e57e88
ebrahimpaki <ebrahimpaki@gmail.com>
2023-07-03 06:58:15 +0500

    update

--- class3.dart
+++ class3.dart
@@ -1,5 +1,5 @@
 void main(){
-  // String a = "sub se pehle github download kre ge";
+  // String a = "sub se pehle github download kre ge";   1
   // String b = "phir id banaye ge yani apna account banaye ge git mai ";
   // String c = """jab bhi hum git mai new account banate hai us mai us waqt sirf ye 2 file chalate hai
   //             system mai wo ye hai ye file sirf 1st time chalye ge""";
@@ -14,12 +14,16 @@ void main(){
   //  String l = "git push -u origin master";
   // print(a+b+c+d+e+f+g+h+i+j+k+l); 
 
-  String m =("project ko update krne ka tariqa");
-  String n = """jab kisi file ko change kre ge to m likha hua aay ga or new file add kre ge to a
-              likha hua a jaye ga""";
-  String o =  "git add .";
-  String p = "git commit -m (message)" ;           // message ki esample niche de hue hai
-  //create class3.dart & update class2.dart" ;  //-m k baad commit inverted comma mai likhna haiadd
-  String q =  "git push     update krne k liye pura push -u nahi likhna q k branch pata hai isko ";
- print(m+n+o+p+q);
+//   String m =("project ko update krne ka tariqa");      //2
+//   String n = """jab kisi file ko change kre ge to m likha hua aay ga or new file add kre ge to a
+//               likha hua a jaye ga""";
+//   String o =  "git add .";
+//   String p = "git commit -m (message)" ;           // message ki esample niche de hue hai
+//   //create class3.dart & update class2.dart" ;  //-m k baad commit inverted comma mai likhna haiadd
+//   String q =  "git push     update krne k liye pura push -u nahi likhna q k branch pata hai isko ";
+//  print(m+n+o+p+q);
+
+    String r = "jub bhi hum git se kisi durse system mai file save kre ge us ka tariqa ye hai";
+    String s = "first cmd ya git bach mai (cd desktop) jaha file save krni hai location";
+    String t =   "second (git clone) or link copy kr k past krna hai cmd ya git bach mai";
 }
